9e8e214497112e369ce8d372b95c9bd3137cfb74
Bram-0316 <bram.boudewijn@student.graafschapcollege.nl>
2022-03-31 10:40:48 +0200

    m

--- PcBouwen/public/Custom.php
+++ PcBouwen/public/Custom.php
@@ -1,9 +1,29 @@
 <?php
 require_once "header.php";
+require_once ('../src/databaseFunctions.php');
+require_once ('../src/userFunctions.php');
+$behuizingen = db_getData('SELECT * FROM `componenten` WHERE `Type` = "Behuizing"');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Custom builds     </h1>
 
-<h1>Custom builds     </h1>
-
+        <select name="ComponentSelect">
+            <?php
+                while($behuizing = $behuizingen->fetch_assoc()){
+            ?>
+                <option value=""><?php echo $behuizing['Merk'] ." ". $behuizing['Serie']  ." ". $behuizing['Kleur'] ." ". $behuizing['Prijs'];?></option>
+            <?php } ?>
+        </select>
+</body>
+</html>
 
 
 
@@ -11,3 +31,5 @@ require_once "header.php";
 <?php
 require_once "footer.php";
 ?>
+
+
--- PcBouwen/public/inlog.php
+++ PcBouwen/public/inlog.php
@@ -17,7 +17,7 @@
 <body>
 <div class="login">
 			<h1>Login</h1>
-			<form action="authenticate.php" method="post">
+			<form class="" action="authenticate.php" method="post">
 				<label for="username">
 					<i class="fas fa-user"></i>
 				</label>
