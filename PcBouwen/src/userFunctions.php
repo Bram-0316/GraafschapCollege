e816d4c4d8f802a949db538056b49a5abc538ced
Bram-0316 <bram.boudewijn@student.graafschapcollege.nl>
2022-04-01 12:19:59 +0200

    w

--- PcBouwen/src/userFunctions.php
+++ PcBouwen/src/userFunctions.php
@@ -2,8 +2,13 @@
 require_once "databaseFunctions.php";
 
 function registerUser($username, $password, $email){
-    $result = db_insertData("INSERT INTO accounts (username, password, email) VALUES ('$username', '$password', '$email' )");
-    return $result;
+
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+    $result = db_insertData("INSERT INTO accounts (username, password, email) VALUES ('$username', '$hashed_password', '$email' )");
+
+
+    return $hashed_password;
 }
 function getUser($email, $password){
     $result = db_getData("SELECT * FROM users WHERE email = '" . $email . "' AND password = '" . $password . "'");
